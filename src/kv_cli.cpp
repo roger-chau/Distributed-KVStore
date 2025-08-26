f013b3e11bdd9a5b09836cefde5bcbb5ca81669c
Roger Chau <54104821+roger-chau@users.noreply.github.com>
2025-08-26 11:06:39 -0700

    Directory check

--- src/kv_cli.cpp
+++ src/kv_cli.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <filesystem>
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    for (int i = 0; i < argc; ++i) { // Print CLI arguments
-        std::cout << argv[i] << std::endl;
-    }
+void printUsageError() {
+    cerr << "ERROR: No Database Path Provided \n"
+              << "Usage:\n"
+              << "  ./KVStore <database-path>\n\n"
+              << "Example:\n"
+              << "  ./KVStore database.log\n\n";
+    exit(1);
+}
 
+void printPathError() {
+    cerr << "ERROR: Invalid Path to Database Directory";
+    exit(1);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) printUsageError();
+    else if (filesystem::is_directory(argv[1])) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
     return 0;
 }
